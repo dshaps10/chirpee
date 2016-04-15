bccc881911f6e9cd47df04f15bd56e426a94d139
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-04-14 19:39:12 -0700

    Add tweet creation form and route to process tweets

---
+++ app/controllers/tweets.rb
@@ -0,0 +1,9 @@
+get '/users/:user_id/new_tweet' do
+	erb :tweet
+end
+
+post '/users/:user_id/new_tweet' do
+	user = User.find(session[:user_id])
+	user.tweets.create(body: params[:body])
+	redirect '/users/:user_id/profile'
+end
