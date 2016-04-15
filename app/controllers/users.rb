110bc9ea02f2f0e8c2c15d276be5c9d46205204f
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-04-14 19:59:58 -0700

    Change routes to compensate for user session

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,7 +6,7 @@ post 'login' do
 	user = User.where(email: params[:email]).first
 	if user && user.password == params[:password]
 		login(user)
-		redirect '/users/:user_id/feed'
+		redirect "/users/#{user.id}/feed"
 	else 
 		@login_failed = true
 		erb :login
@@ -18,16 +18,24 @@ get '/logout' do
 	redirect '/'
 end 
 
-get '/users/:user_id/feed' do 
-	redirect '/' unless current_user
-	flash[:warning] = "Sorry this page is private"
-	erb :feed
+get '/users/:user_id/feed' do
+	@user = User.find(params[:user_id])
+	if current_user == @user
+		erb :feed
+	else
+		flash[:warning] = "Sorry this page is private"
+		redirect '/'
+	end
 end 
 
 get '/users/:user_id/profile' do 
-	redirect '/' unless current_user
-	flash[:warning] = "Sorry this page is private"
-	erb :profile
+	@user = User.find(params[:user_id])
+	if current_user == @user
+		erb :feed
+	else
+		flash[:warning] = "Sorry this page is private"
+		redirect '/'
+	end
 end 
 
 
